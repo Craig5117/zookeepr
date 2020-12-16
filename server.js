a6b8c697b623f9a0d0e8911a7f837a3b212b820e
Craig5117 <craig.bennett5117@gmail.com>
2020-12-16 15:19:30 -0500

    added parsing for incoming data

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 const { animals } = require('./data/animals.json')
 
 function capitalizeFirstLetter(string) {
@@ -77,6 +81,12 @@ app.get('/api/animals/:id', (req, res) => {
         else {
             res.sendStatus(404);
         }
+});
+
+app.post('/api/animals', (req, res) => {
+    // req.body is where our incoming content will be
+    console.log(req.body);
+    res.json(req.body);
 });
 
 app.listen(PORT, () => {
